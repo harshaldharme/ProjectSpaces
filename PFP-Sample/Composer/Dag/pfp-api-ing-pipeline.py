9d456cf56fc613497b3e5ab1d7148b2000eb9230
Harshal Dharme <32070236+harshaldharme@users.noreply.github.com>
2024-11-24 22:46:35 +0000

    Update pfp-api-ing-pipeline.py

--- PFP-Sample/Composer/Dag/pfp-api-ing-pipeline.py
+++ PFP-Sample/Composer/Dag/pfp-api-ing-pipeline.py
@@ -1,1 +1,26 @@
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import timedelta
 
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(0),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'pfp-api-ingestion-pipeline',
+    default_args=default_args,
+    description='Dag to ingest the data from API into BigQuery',
+    schedule_interval=None,
+    dagrun_timeout=timedelta(minutes=20),
+)
+
+t1 = BashOperator(
+    task_id='Setup Environment',
+    bash_command='echo Hi',
+    dag=dag,
+    depends_on_past=False,
+    priority_weight=2**31 - 1,
+    do_xcom_push=False)
